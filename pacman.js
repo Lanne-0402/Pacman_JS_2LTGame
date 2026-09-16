30de993492240ae606f5af46b343beaea0d78a6e
Lanne-0402 <honglanbd2gmail.com>
2026-09-16 23:29:30 +0700

    Update assets, index, css, js ver

---
+++ pacman.js
@@ -0,0 +1,150 @@
+//board
+let board;
+const rowCount = 21;
+const columnCount = 19;
+const tileSize = 32;
+const boardWidth = columnCount * tileSize;
+const boardHeight = rowCount * tileSize;
+let context;
+
+//images
+let blueGhostImage;
+let orangeGhostImage;
+let pinkGhostImage;
+let redGhostImage;
+let pacmanUpImage;
+let pacmanDownImage;
+let pacmanLeftImage;
+let pacmanRightImage;
+let wallImage;
+
+window.onload = function() {
+    board = document.getElementById("board");
+    board.height = boardHeight;
+    board.width = boardWidth;
+    context = board.getContext("2d"); //Dung context de ve cac hinh anh len canvas
+
+    loadImages();
+    loadMap();
+    // console.log(walls.size);
+    // console.log(foods.size);
+    // console.log(ghosts.size);
+    update();
+} 
+
+const tileMap = [
+    "XXXXXXXXXXXXXXXXXXX",
+    "X        X        X",
+    "X XX XXX X XXX XX X",
+    "X                 X",
+    "X XX X XXXXX X XX X",
+    "X    X       X    X",
+    "XXXX XXXX XXXX XXXX",
+    "OOOX X       X XOOO",
+    "XXXX X XXrXX X XXXX",
+    "O       bpo       O",
+    "XXXX X XXXXX X XXXX",
+    "OOOX X       X XOOO",
+    "XXXX X XXXXX X XXXX",
+    "X        X        X",
+    "X XX XXX X XXX XX X",
+    "X  X     P     X  X",
+    "XX X X XXXXX X X XX",
+    "X    X   X   X    X",
+    "X XXXXXX X XXXXXX X",
+    "X                 X",
+    "XXXXXXXXXXXXXXXXXXX" 
+];
+
+const walls = new Set();
+const foods = new Set();
+const ghosts = new Set();
+let pacman;
+
+function loadImages() {
+    wallImage = new Image();
+    wallImage.src = "assets/wall.png";
+
+    blueGhostImage = new Image();
+    blueGhostImage.src = "assets/blueGhost.png";
+    orangeGhostImage = new Image();
+    orangeGhostImage.src = "assets/orangeGhost.png";
+    pinkGhostImage = new Image();
+    pinkGhostImage.src = "assets/pinkGhost.png";
+    redGhostImage = new Image();
+    redGhostImage.src = "assets/redGhost.png";
+
+    pacmanUpImage = new Image();
+    pacmanUpImage.src = "assets/pacmanUp.png";
+    pacmanDownImage = new Image();
+    pacmanDownImage.src = "assets/pacmanDown.png";
+    pacmanLeftImage = new Image();
+    pacmanLeftImage.src = "assets/pacmanLeft.png";
+    pacmanRightImage = new Image();
+    pacmanRightImage.src = "assets/pacmanRight.png";
+}
+
+function loadMap() {
+    walls.clear();
+    foods.clear();
+    ghosts.clear();
+    
+    for (let r = 0; r < r < rowCount; r++) {
+        for (let c = 0; c <columnCount; c++) {
+            const row = tileMap[r];
+            const tileMapChar = row[c];
+
+            const x = c*tileSize;
+            const y = r*tileSize;
+
+            if (tileMapChar == 'X') {
+                const wall = new Block(wallImage, x, y, tileSize, tileSize);
+                walls.add(wall);
+            }
+            else if (tileMapChar == 'b') {
+                const ghost = new Block(blueGhostImage, x, y, tileSize, tileSize);
+                ghosts.add(ghost);
+            }
+            else if (tileMapChar == 'o') {
+                const ghost = new Block(orangeGhostImage, x, y, tileSize, tileSize);
+                ghosts.add(ghost);
+            }
+            else if (tileMapChar == 'p') {
+                const ghost = new Block(pinkGhostImage, x, y, tileSize, tileSize);
+                ghosts.add(ghost);
+            }
+            else if (tileMapChar == 'r') {
+                const ghost = new Block(redGhostImage, x, y, tileSize, tileSize);
+                ghosts.add(ghost);
+            }
+            else if (tileMapChar == 'P') {
+                pacman = new Block(pacmanRightImage, x, y, tileSize, tileSize);
+            }
+            else if (tileMapChar == ' ') {
+                const food = new Block(null, x+14, y+14, 4, 4);
+                foods.add(food);
+            }
+        }
+    }
+}
+
+function update() {
+    draw();
+
+}
+
+function draw() {
+    
+}
+
+class Block {
+    constructor(image, x, y, width, height) {
+        this.image = image;
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.startX = x;
+        this.startY = y;
+    }
+}
